721d3249e45c58a9dcbfa103d850f28ead08fedb
Niku-Ya <deathrenewed@gmail.com>
2016-02-26 22:44:10 -0800

    Stuff

--- src/Homework/Ranking.java
+++ src/Homework/Ranking.java
@@ -1,9 +1,32 @@
 package Homework;
 
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.MongoClient;
+
 public class Ranking {
 	// TF = total count of the word in document / total words in document
 	// IDF = log total documents looking at / total # of documents containing the word
 	// The word will be based on the search query
 	// Need to crawl desktop
+	DB database;
+	DBCollection table = database.getCollection("urlpages");
+	DBCollection index = database.getCollection("index");
+	
+	public Ranking(DB database) {
+		this.database = database;
+	}
+
+
+
+	public void TFIDF ( String term ) {
+		DBObject object = index.findOne(new BasicDBObject("word", term));
+		
+		
+	}
+	
+	
 
 }
